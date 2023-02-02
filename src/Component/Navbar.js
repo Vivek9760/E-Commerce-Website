184225324fb86fa5ca99e04392ebae69ea1d3fb7
VivekSaini <vvivekssaini@gmail.com>
2023-02-02 19:57:10 +0530

    Day 1 - update Navbar

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -12,10 +12,10 @@ function Navbar(){
          <div className='container_2'>
          <div className='heading-container'>
                 <ButtonGroup variant='outlined' size='small' color='success'>
-                    <Button id="h1" variant='contained' >Heading 1</Button>
-                    <Button id="h2" >Heading 4</Button>
-                    <Button id="h3" >Heading 2</Button>
-                    <Button id="h4" >Heading 3</Button>
+                    <Link className='link' to="/"><Button id="h1" variant='contained' >Heading 1</Button></Link>
+                    <Link className='link' to="/"><Button id="h2" >Heading 4</Button></Link>
+                    <Link className='link' to="/"><Button id="h3" >Heading 2</Button></Link>
+                    <Link className='link' to="/"><Button id="h4" >Heading 3</Button></Link>
                 </ButtonGroup>
             </div>
          </div>
@@ -24,10 +24,10 @@ function Navbar(){
         <div className='container_3'>
             <div className='btn-container'>
                 <div className='btn-main-container'>
-               <Link to="/signup"><Button id="btn1" size='small' color='error' variant='contained'> Sign up </Button></Link>
+               <Link className='link' to="/signup"><Button id="btn1" size='small' color='error' variant='contained'> Sign up </Button></Link>
                 </div>
                 <div className='btn-main-container'>
-                <Link to="/login"><Button id="btn2" size='small' color='error' variant='contained'>Log In</Button></Link>
+                <Link className='link' to="/login"><Button id="btn2" size='small' color='error' variant='contained'>Log In</Button></Link>
                 </div>
             </div>
 

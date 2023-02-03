ffd909531963a453ecd4a020a3c7749237f217a3
VivekSaini <vvivekssaini@gmail.com>
2023-02-04 02:50:27 +0530

    Day 2 - add private component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './Component/Login';
 import Navbar from './Component/Navbar';
+import PrivateComponent from './Component/PrivateComponent';
 import Signup from './Component/Signup';
 
 function App() {
@@ -9,9 +10,11 @@ function App() {
    <BrowserRouter>
    <Navbar />
    <Routes>
+    <Route element={<PrivateComponent />}>
+    <Route path='/' element={<h1>Profile</h1>} />
+    </Route>
     <Route path='/login' element={<Login />} />
     <Route path='/signup' element={<Signup />} />
-
    </Routes>
    </BrowserRouter>
    </>
---
+++ src/Component/PrivateComponent.js
@@ -0,0 +1,10 @@
+import { Outlet, Navigate } from "react-router-dom";
+
+const PrivateComponent = () =>{
+
+    return(
+            localStorage.getItem('user')?<Outlet />:<Navigate to='signup'/>       
+    )
+}
+
+export default PrivateComponent
